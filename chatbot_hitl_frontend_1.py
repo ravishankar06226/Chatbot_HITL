2a7ec3d1538ba2a4bbbc5b98a5013618abd7f995
Ravi Shankar <56543452+ravishankar06226@users.noreply.github.com>
2026-06-29 14:47:15 +0530

    Update chatbot_hitl_frontend_1.py

--- chatbot_hitl_frontend_1.py
+++ chatbot_hitl_frontend_1.py
@@ -31,7 +31,6 @@ with st.sidebar:
             data=file,
             file_name="downloaded_image.png",
             mime="image/png")
-            st.session_state.file=''
 
 # Display chat history
 for msg in st.session_state.messages:
