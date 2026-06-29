8e7b43b5f48ef02f7deaa2777d9fcafb07a1327f
Ravi Shankar <56543452+ravishankar06226@users.noreply.github.com>
2026-06-29 14:38:12 +0530

    Update chatbot_hitl_frontend_1.py

--- chatbot_hitl_frontend_1.py
+++ chatbot_hitl_frontend_1.py
@@ -1,8 +1,10 @@
 import streamlit as st
 from chatbot_hitl_backend_1 import chatbot
-from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_core.messages import HumanMessage
 from langgraph.types import interrupt, Command
 import uuid
+import os
+import tempfile
 
 # Initialize session state
 if "thread_id" not in st.session_state:
@@ -16,16 +18,35 @@ if "waiting_for_hitl" not in st.session_state:
 
 if "hitl_prompt" not in st.session_state:
     st.session_state.hitl_prompt = None
-
+if "file" not in st.session_state:
+    st.session_state.file=''
 
 st.title("LangGraph Chatbot")
+
+with st.sidebar:
+    uploaded_file = st.file_uploader("Choose a file", type=["txt", "csv", "xlsx", "xls", "tsv"])
+    if os.path.isfile(st.session_state.file+".png"):
+        with open(st.session_state.file+".png", "rb") as file:
+            st.download_button(label="Download PNG Image",
+            data=file,
+            file_name="downloaded_image.png",
+            mime="image/png")
+            st.session_state.file=''
 
 # Display chat history
 for msg in st.session_state.messages:
     with st.chat_message(msg["role"]):
         st.write(msg["content"])
 
-
+if st.session_state.file == '':
+    if uploaded_file is not None:
+        # Create a temporary file that persists long enough to run the command
+        with tempfile.NamedTemporaryFile(delete=False, suffix=os.path.splitext(uploaded_file.name)[-1]) as temp_file:
+            temp_file.write(uploaded_file.getvalue())
+            temp_path = temp_file.name  # This is the physical path string
+            #print(temp_path)
+            st.session_state.file=temp_path
+print(st.session_state.file)
 # Normal chat input
 if not st.session_state.waiting_for_hitl:
     user_input = st.chat_input("Type your message...")
@@ -39,7 +60,8 @@ if not st.session_state.waiting_for_hitl:
             st.write(user_input)
 
         state = {
-            "messages": [HumanMessage(content=user_input)]
+            "messages": [HumanMessage(content=user_input)],
+            "file":st.session_state.file
         }
 
         result = chatbot.invoke(
@@ -67,8 +89,8 @@ if not st.session_state.waiting_for_hitl:
 
             with st.chat_message("assistant"):
                 st.write(bot_response)
-
             st.rerun()
+
 
 
 # HITL approval section
