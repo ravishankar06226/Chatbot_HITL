486b5cce102310465e01c37638a5af29d910244e
Ravi Shankar <56543452+ravishankar06226@users.noreply.github.com>
2026-07-10 15:55:47 +0530

    Update chatbot_hitl_frontend_1.py
    dge/pathway added

--- chatbot_hitl_frontend_1.py
+++ chatbot_hitl_frontend_1.py
@@ -5,6 +5,7 @@ from langgraph.types import interrupt, Command
 import uuid
 import os
 import tempfile
+import glob
 
 # Initialize session state
 if "thread_id" not in st.session_state:
@@ -18,39 +19,62 @@ if "waiting_for_hitl" not in st.session_state:
 
 if "hitl_prompt" not in st.session_state:
     st.session_state.hitl_prompt = None
-if "file" not in st.session_state:
-    st.session_state.file=""
+if "files" not in st.session_state:
+    st.session_state.files=[]
 
-st.title("LangGraph Chatbot")
+st.title("Bioinformatics Assistant")
 
 
 with st.sidebar:
-    uploaded_file = st.file_uploader("Choose a file", type=["txt", "csv", "xlsx", "xls", "tsv"])
-    if uploaded_file is not None:
-        if os.path.isfile(st.session_state.file+".png"):
-            with open(st.session_state.file+".png", "rb") as file:
-                st.download_button(label="Download PNG Image",
-                data=file,
-                file_name="downloaded_image.png",
-                mime="image/png")
+    uploaded_files = st.file_uploader("Choose a file", type=["xlsx", "xls"],accept_multiple_files=True)
+    
+if (uploaded_files!=[] or uploaded_files!=None) and st.session_state.files==[]:
+    for each_file in uploaded_files:
+        with tempfile.NamedTemporaryFile(delete=False, suffix=os.path.splitext(each_file.name)[-1]) as temp_file:
+            temp_file.write(each_file.getvalue())
+            temp_path = temp_file.name  # This is the physical path string
+            st.session_state.files.append(temp_path)
+
+if (uploaded_files==[] or uploaded_files==None) and st.session_state.files!=[]:
+    for remove_files in st.session_state.files:
+        os.remove(remove_files)
+    st.session_state.files=[]
+with st.sidebar:
+    if (uploaded_files!=[] or uploaded_files!=None) and st.session_state.files!=[]:
+        if len(glob.glob(st.session_state.files[0]+"_*")) == 1:
+            if os.path.isfile(glob.glob(st.session_state.files[0]+"_*")[0]):
+                with open(glob.glob(st.session_state.files[0]+"_*")[0], "rb") as file:
+                    st.download_button(label="Download Processed File",
+                    data=file,
+                    file_name="Downloaded_file."+glob.glob(st.session_state.files[0]+"_*")[0].split(".")[-1],
+                    mime="application/octet-stream")
+
+
+
+
+with st.sidebar:
+    if (uploaded_files!=[] or uploaded_files!=None) and st.session_state.files!=[]:
+        if len(glob.glob(st.session_state.files[0]+"_*")) == 2:
+            if os.path.isfile(glob.glob(st.session_state.files[0]+"_*")[0]):
+                with open(glob.glob(st.session_state.files[0]+"_*")[0], "rb") as file:
+                    st.download_button(label="Download Processed File",
+                    data=file,
+                    file_name="Downloaded_file."+glob.glob(st.session_state.files[0]+"_*")[0].split(".")[-1],
+                    mime="application/octet-stream")
+
+            if os.path.isfile(glob.glob(st.session_state.files[0]+"_*")[1]):
+                with open(glob.glob(st.session_state.files[0]+"_*")[1], "rb") as file:
+                    st.download_button(label="Download Processed File",
+                    data=file,
+                    file_name="Downloaded_file."+glob.glob(st.session_state.files[0]+"_*")[1].split(".")[-1],
+                    mime="application/octet-stream")
+
 
 # Display chat history
 for msg in st.session_state.messages:
     with st.chat_message(msg["role"]):
         st.write(msg["content"])
 
-    # Create a temporary file that persists long enough to run the command
-if uploaded_file is not None:
-    if st.session_state.file=="":
-        with tempfile.NamedTemporaryFile(delete=False, suffix=os.path.splitext(uploaded_file.name)[-1]) as temp_file:
-            temp_file.write(uploaded_file.getvalue())
-            temp_path = temp_file.name  # This is the physical path string
-            #print(temp_path)
-            st.session_state.file=temp_path
-if uploaded_file is None:
-    st.session_state.file=""
-
-print(st.session_state.file)
 # Normal chat input
 if not st.session_state.waiting_for_hitl:
     user_input = st.chat_input("Type your message...")
@@ -61,11 +85,11 @@ if not st.session_state.waiting_for_hitl:
         )
 
         with st.chat_message("user"):
-            st.write(user_input+st.session_state.file)
+            st.write(user_input)
 
         state = {
             "messages": [HumanMessage(content=user_input)],
-            "file":st.session_state.file
+            "files":st.session_state.files
         }
 
         result = chatbot.invoke(
@@ -104,9 +128,9 @@ if st.session_state.waiting_for_hitl:
     col1, col2 = st.columns(2)
 
     with col1:
-        if st.button("Approve"):
+        if st.button("Human"):
             result = chatbot.invoke(
-                Command(resume="yes"),
+                Command(resume="human"),
                 config={
                     "configurable": {
                         "thread_id": st.session_state.thread_id
@@ -126,9 +150,9 @@ if st.session_state.waiting_for_hitl:
             st.rerun()
 
     with col2:
-        if st.button("Reject"):
+        if st.button("Mouse"):
             result = chatbot.invoke(
-                Command(resume="no"),
+                Command(resume="mouse"),
                 config={
                     "configurable": {
                         "thread_id": st.session_state.thread_id
